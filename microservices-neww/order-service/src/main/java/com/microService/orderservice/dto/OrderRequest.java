bcb67b8d443943d0dd38f6dfa3e2bddacbcf49e3
ABDELMOUIZ1 <abdelmoize1234@gmail.com>
2024-08-09 20:44:40 +0100

    Initial commit for a shop project using microservices architecture

---
+++ microservices-neww/order-service/src/main/java/com/microService/orderservice/dto/OrderRequest.java
@@ -0,0 +1,14 @@
+package com.microService.orderservice.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
---
+++ microservices-neww/order-service/src/main/java/com/microService/orderservice/service/OrderService.java
@@ -0,0 +1,86 @@
+package com.microService.orderservice.service;
+
+import com.microService.orderservice.dto.InventoryResponse;
+import com.microService.orderservice.dto.OrderLineItemsDto;
+import com.microService.orderservice.dto.OrderRequest;
+import com.microService.orderservice.event.OrderPlacedEvent;
+import com.microService.orderservice.model.Order;
+import com.microService.orderservice.model.OrderLineItems;
+import com.microService.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
+
+    public String placeOrder(OrderRequest orderRequest) {
+        System.out.println(orderRequest);
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service, and place order if product is in
+        // stock
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+
+        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+
+            //inventoryServiceLookup.tag("call", "inventory-service");
+            InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if(allProductsInStock){
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        } finally {
+            inventoryServiceLookup.end();
+        }
+    }
+
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
